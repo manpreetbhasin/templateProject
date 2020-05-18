36b53e1defa2f6a23e610a6763c5cd736f60b8da
Manpreet Bhasin <manpreetbhasin@gmail.com>
2020-05-17 18:14:11 -0700

    WIP Adding images and cleaning up main page

--- phantom/react_html5up_phantom/src/components/footer.js
+++ phantom/react_html5up_phantom/src/components/footer.js
@@ -4,38 +4,38 @@ export default class Footer extends React.Component {
     render() {
         return (
             <footer id="footer">
-                <div classname="inner">
+                <div className="inner">
                     <section>
                         <h2>Get in touch</h2>
                         <form method="post" action="#">
-                            <div classname="field half first">
+                            <div className="field half first">
                                 <input type="text" name="name" id="name" placeholder="Name"/>
                             </div>
-                            <div classname="field half">
+                            <div className="field half">
                                 <input type="email" name="email" id="email" placeholder="Email"/>
                             </div>
-                            <div classname="field">
+                            <div className="field">
                                 <textarea name="message" id="message" placeholder="Message" defaultValue={""}/>
                             </div>
-                            <ul classname="actions">
-                                <li><input type="submit" defaultValue="Send" classname="special"/></li>
+                            <ul className="actions">
+                                <li><input type="submit" defaultValue="Send" className="special"/></li>
                             </ul>
                         </form>
                     </section>
                     <section>
                         <h2>Follow</h2>
-                        <ul classname="icons">
-                            <li><a href="#" classname="icon style2 fa-twitter"><span classname="label">Twitter</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-facebook"><span classname="label">Facebook</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-instagram"><span classname="label">Instagram</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-dribbble"><span classname="label">Dribbble</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-github"><span classname="label">GitHub</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-500px"><span classname="label">500px</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-phone"><span classname="label">Phone</span></a></li>
-                            <li><a href="#" classname="icon style2 fa-envelope-o"><span classname="label">Email</span></a></li>
+                        <ul className="icons">
+                            <li><a href="#" className="icon style2 fa-twitter"><span className="label">Twitter</span></a></li>
+                            <li><a href="#" className="icon style2 fa-facebook"><span className="label">Facebook</span></a></li>
+                            <li><a href="#" className="icon style2 fa-instagram"><span className="label">Instagram</span></a></li>
+                            <li><a href="#" className="icon style2 fa-dribbble"><span className="label">Dribbble</span></a></li>
+                            <li><a href="#" className="icon style2 fa-github"><span className="label">GitHub</span></a></li>
+                            <li><a href="#" className="icon style2 fa-500px"><span className="label">500px</span></a></li>
+                            <li><a href="#" className="icon style2 fa-phone"><span className="label">Phone</span></a></li>
+                            <li><a href="#" className="icon style2 fa-envelope-o"><span className="label">Email</span></a></li>
                         </ul>
                     </section>
-                    <ul classname="copyright">
+                    <ul className="copyright">
                         <li>© Untitled. All rights reserved</li>
                         <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
                     </ul>
